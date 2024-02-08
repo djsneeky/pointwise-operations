5a4894fe82ee37bac39669115bb777b7d3b475b6
djsneeky <djfurtweng@gmail.com>
2024-02-07 23:45:19 -0600

    gamma correct a gamma corrected image

--- gamma.py
+++ gamma.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from PIL import Image
 import sys
 from pathlib import Path
 
@@ -32,21 +33,54 @@ def display_stripes(gray_level):
     plt.show()
 
     return array
+
+def gamma_correct_linear(linear_img: np.array, gamma: float):
+    # original
+    # y = 255 * (x / 255) ** gamma
+    # apply inverted equation
+    x = linear_img
+    y = 255 * (x / 255) ** (1 / gamma)
+
+    return y
+
+def gamma_correct(gamma_corrected_img: np.array, gamma: float, desired_gamma: float):
+    # convert gamma corrected image back to linear
+    x = gamma_corrected_img
+    linear_img = 255 * (x / 255) ** (gamma)
+    
+    # convert linear image back to gamma corrected
+    y = gamma_correct_linear(linear_img, desired_gamma)
+    
+    return y
+    
     
 def main():
     initial_gray_level = 127
-    initial_image = display_stripes(initial_gray_level)
+    # initial_image = display_stripes(initial_gray_level)
     
     matching_gray_level = 170
     # Display the image with the matching gray level
-    matching_image = display_stripes(matching_gray_level)
+    # matching_image = display_stripes(matching_gray_level)
 
     # Print the matching gray level
     print("Matching Gray Level:", matching_gray_level)
 
     # Calculate gamma
-    gamma = np.log(0.5) / np.log(matching_gray_level / 255)
-    print("gamma: ", gamma)
+    monitor_gamma = np.log(0.5) / np.log(matching_gray_level / 255)
+    print("monitor gamma: ", monitor_gamma)
+    
+    im = Image.open('linear.tif')
+    x = np.array(im)
+
+    y = gamma_correct_linear(x, monitor_gamma)
+    plt.imsave('linear_gamma_corrected.png', y, cmap='gray')
+    
+    im = Image.open('gamma15.tif')
+    x = np.array(im)
+    
+    y = gamma_correct(x, 1.5, monitor_gamma)
+    plt.imsave('gamma15_gamma_corrected.png', y, cmap='gray')
+    
     
 if __name__ == "__main__":
     main()
